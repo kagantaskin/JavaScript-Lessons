b0de2b9b9d6243d744321e7b4833e4558562f6ed
Toprak <hello@toprak.dev>
2020-05-02 00:45:48 +0300

    Var vs Let vs Const

--- JS Basics/1) Var X;/script.js
+++ JS Basics/1) Var X;/script.js
@@ -1,14 +1,38 @@
 var firstName = 'Johnny';
+
 console.log(firstName);
 
 var lastName = 'Cash';
 var age = 42;
 
 var fullAge = true;
+
 console.log(fullAge);
 
 var job;
+
 console.log(job);
 
 job = 'Guitarist';
+
 console.log(job);
+
+//Var
+
+var publicVariable = 0;
+
+if (1 === 1) {
+    console.log(publicVariable);
+    publicVariable = 1;
+}
+
+console.log(publicVariable);
+
+//Let
+
+if (1 === 1) {
+    let limitedVariable = 0;
+    console.log(limitedVariable);
+}
+
+console.log(limitedVariable);
