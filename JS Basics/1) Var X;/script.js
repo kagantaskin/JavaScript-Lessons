9d7ebefc34dab1d287b9226ab0ac5afc47cd8dd1
kagantaskin <31819830+kagantaskin@users.noreply.github.com>
2020-05-02 00:28:07 +0300

    Learned Variables

---
+++ JS Basics/1) Var X;/script.js
@@ -0,0 +1,14 @@
+var firstName = 'Johnny';
+console.log(firstName);
+
+var lastName = 'Cash';
+var age = 42;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Guitarist';
+console.log(job);
