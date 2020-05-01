405abfa089edeb4b670e18c6928f1abdb0ad24d3
kagantaskin <31819830+kagantaskin@users.noreply.github.com>
2020-05-02 00:28:55 +0300

    Operator Precedence

---
+++ JS Basics/4) Op Precedence/script.js
@@ -0,0 +1,22 @@
+var now = 2020;
+var yearJohn = 1989;
+var fullAge = 25;
+
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var avAge = (ageJohn + ageMark) / 2;
+console.log(avAge);
+
+var x, y;
+	x = y = (3 + 5) * 4 - 6;
+console.log(x, y);
+
+	x = x * 2;
+		console.log(x);
+	x = x - 10;
+		console.log(x);
+	x++;
+		console.log(x);
