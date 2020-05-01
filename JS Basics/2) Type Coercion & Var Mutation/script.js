f60f67451daba0a03559589dcb9a09874bfa8786
kagantaskin <31819830+kagantaskin@users.noreply.github.com>
2020-05-02 00:28:24 +0300

    Type Coercions

---
+++ JS Basics/2) Type Coercion & Var Mutation/script.js
@@ -0,0 +1,20 @@
+var firstName = 'Johnny';
+var age = 42;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+
+job = 'Guitarist';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Var Mutation
+age = 'forty two';
+job = 'Lawyer';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('Whats his last name?');
+console.log(lastName);
