624fa6c7bade82a7388b2c5d66e2f50cfcd6b1d9
kagantaskin <31819830+kagantaskin@users.noreply.github.com>
2020-05-18 05:25:52 +0300

    Chat Bot 2000

--- Message Bot/script.js
+++ Message Bot/script.js
@@ -1,30 +1,166 @@
-var userNameOnStorage = localStorage.getItem("usersName");
+const userNameOnStorage = localStorage.getItem("userName");
 
-function checkForName() {
-
-	if (userNameOnStorage === null) {
-		document.write("<h1>Please set a name for yourself</h1>");
-		console.log('please set a name for yourself');
-		importUsersName();
+function Load() {
+	if (userNameOnStorage) {
+		document.querySelector('.nameGetter').style.display = 'none';
+		document.querySelector('.shoppingListMain').style.display = 'none';
+		document.querySelector('.chatBoxMain').style.display = 'block';
+		console.log('Your name is ' + userNameOnStorage + '\nMake a shopping list for me');
 	} else {
-		console.log('your name already set');
-		console.log(userNameOnStorage);
+		console.log("Hello Stranger. What's Your Name\n");
+		getName();
 	}
 }
 
-checkForName();
+Load();
 
-function importUsersName() {
-	if (document.getElementById('userNameForStorage')) {	
-		var userNameForStorage = document.getElementById('userNameForStorage').value;
-		localStorage.setItem('usersName', userNameForStorage);
+function getName() {
+	document.querySelector('.nameGetter').style.display = 'block';
+	document.querySelector('.chatBoxMain').style.display = 'none';
+	document.querySelector('.shoppingListMain').style.display = 'none';
+
+	let name = document.getElementById('getUserName').value;
+
+	if (name) {
+		importName(name);
+	} else {
+		console.log("You need a write your name on the Textbox\nEx. My name is 'Mahmut'");
+	}
+}
+
+function importName(name) {
+	if (name.split("'")[1] === undefined) {
+		console.log("Hint: My name is 'Mahmut'");
+	} else {
+		localStorage.setItem('userName', name.split("'")[1]);
 		location.reload();
-	} else {
-		console.log('invalid');
 	}
 }
 
-function deleteUsersName() {
-	localStorage.removeItem('usersName');
+function deleteName() {
+	localStorage.removeItem('userName');
 	location.reload();
+}
+
+function getMessageWithKey() {
+	var getMessageButton = document.getElementById("messagesSent");
+
+	getMessageButton.addEventListener("keyup", function(event) {
+		if (event.keyCode === 13) {
+	    	document.getElementById("messageGetter").click();	
+		}
+	})
+}
+
+getMessageWithKey();
+
+function getMessage() {
+	let getMessage = document.querySelector('.messagesSent').value;
+	var getMessageDate = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+
+	if (getMessage) {
+		if (botRespond(getMessage)) {
+			document.querySelector('.messagesSent').value = '';
+
+			var messagesList = document.querySelector('.chatMessages');
+			var messagesListBot = document.querySelector('.chatMessages');
+
+			var listForUser = document.createElement('li');
+			var listForBot = document.createElement('li');
+			var listForUserDate = document.createElement('li');
+			var listForBotDate = document.createElement('li');
+
+			var usersMessage = document.createTextNode(userNameOnStorage + ': ' + getMessage);
+			listForUser.setAttribute("name", 'usersMessage');
+			listForUser.setAttribute("class", 'usersMessage');
+			listForUser.appendChild(usersMessage);
+
+			var botsRespond = document.createTextNode('Chat Bot 2000: ' + botRespond(getMessage));
+			listForBot.setAttribute("name", 'botsRespond');
+			listForBot.setAttribute("class", 'botsRespond');
+			listForBot.appendChild(botsRespond);
+
+			var usersMessageDate = document.createTextNode(getMessageDate);
+			listForUserDate.setAttribute("name", 'usersMessage');
+			listForUserDate.setAttribute("class", 'usersMessageDate');
+			listForUserDate.appendChild(usersMessageDate);
+
+			var botsMessageDate = document.createTextNode(getMessageDate);
+			listForBotDate.setAttribute("name", 'botsRespond');
+			listForBotDate.setAttribute("class", 'botsRespondDate');
+			listForBotDate.appendChild(botsMessageDate);
+
+
+			messagesList.appendChild(listForUserDate);
+			messagesList.appendChild(listForUser);
+			messagesListBot.appendChild(listForBotDate);
+			messagesListBot.appendChild(listForBot);
+		}		
+	} else {
+		console.log(userNameOnStorage + "'s message: " + getMessage + '\nBot Responded: Please write a message');
+	}
+}
+
+function createShoppingListItems() {
+	document.querySelector('.chatBoxInputs').style.display = 'none';
+	document.querySelector('.shoppingListMain').style.display = 'block';
+	document.querySelector('.justDeleteName').style.display = 'none';
+
+}
+
+var shoppingList = {  };
+
+function makeShoppingList() {
+	let getshoppingListName = document.querySelector('.shoppingListName').value;
+	let getShoppingItem = document.querySelector('.shoppingItemSent').value;
+	let getShoppingItemsCount = document.querySelector('.shoppingItemsCountSent').value;
+
+	if (getShoppingItem && getShoppingItemsCount) {
+		console.log(getShoppingItem + ' count ' + getShoppingItemsCount);
+		shoppingList[getShoppingItem] = getShoppingItemsCount;
+		localStorage.setItem(getshoppingListName, JSON.stringify(shoppingList));
+	} else {
+		console.log('Please write a Item and the Items Count');
+	}
+}
+
+function backToMessaging() {
+	document.querySelector('.chatBoxInputs').style.display = 'block';
+	document.querySelector('.shoppingListMain').style.display = 'none';	
+	document.querySelector('.messagesSent').value = '';
+}
+
+function listShoppingList() {
+	let getshoppingListName = document.querySelector('.shoppingListName').value;
+	var splitOne, splitSec, retrievedObj;
+	//console.log(shoppingList);
+	retrievedObj = localStorage.getItem(getshoppingListName);
+	splitOne = retrievedObj.split(",");
+	console.log('Ret Obj : ' + retrievedObj);
+	for (var i = 0; i <= splitOne.length - 1; i++) {
+		/*splitOne = retrievedObj.split(",");
+		splitSec = splitOne[i].replace(/"|{|}/g, "");*/
+		console.log((retrievedObj.split(",")[i].replace(/"|{|}/g, "")));
+	}
+}
+
+function clearObj() {
+	//Code
+}
+
+function botRespond(messages) {
+	switch(messages.toLowerCase()) {
+		case 'hey':
+			return 'Hey ' + userNameOnStorage;
+		case 'i love you':
+			return 'I love you too ' + userNameOnStorage;
+		case 'ne basliyo la hayatiniz mi':
+			return 'bekle de gor';
+		case 'make a shopping list for me':
+			return createShoppingListItems();
+		case 'ahmet can babadas':
+			return 'ama koyduk';
+		default:
+			return 'I dont know this command';
+	}
 }
